ff23fd72cf8b8700fba22570817d85433d144348
kwakseobang <iii6602@naver.com>
2025-08-02 11:57:07 +0900

    Setting/#1 : JpaEntity 초기 생성

---
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/member/entity/Member.java
@@ -0,0 +1,9 @@
+package org.kwakmunsu.dingdongpang.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import org.kwakmunsu.dingdongpang.domain.BaseEntity;
+
+@Entity
+public class Member extends BaseEntity {
+
+}
---
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/menu/entity/Menu.java
@@ -0,0 +1,9 @@
+package org.kwakmunsu.dingdongpang.domain.menu.entity;
+
+import jakarta.persistence.Entity;
+import org.kwakmunsu.dingdongpang.domain.BaseEntity;
+
+@Entity
+public class Menu extends BaseEntity {
+
+}
