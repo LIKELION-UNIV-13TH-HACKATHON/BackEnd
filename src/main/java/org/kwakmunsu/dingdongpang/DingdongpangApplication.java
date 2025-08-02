8508e698c4e141097b173a134c3bfe8d8dda2678
kwakseobang <iii6602@naver.com>
2025-08-02 11:03:24 +0900

    git initialize

---
+++ src/main/java/org/kwakmunsu/dingdongpang/DingdongpangApplication.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.dingdongpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingdongpangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingdongpangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kwakmunsu/dingdongpang/DingdongpangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.dingdongpang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdongpangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
