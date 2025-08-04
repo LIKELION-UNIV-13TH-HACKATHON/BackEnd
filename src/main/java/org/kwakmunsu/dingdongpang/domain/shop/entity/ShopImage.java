251c17bf59b7c0bfb7e03aa7745c18beda8ca681
kwakseobang <iii6602@naver.com>
2025-08-04 22:47:11 +0900

    File/#5 : 파일 이동

--- src/main/java/org/kwakmunsu/dingdongpang/domain/shop/entity/ShopImage.java
+++
@@ -1,9 +0,0 @@
-package org.kwakmunsu.dingdongpang.domain.shop.entity;
-
-import jakarta.persistence.Entity;
-import org.kwakmunsu.dingdongpang.domain.BaseEntity;
-
-@Entity
-public class ShopImage extends BaseEntity {
-
-}
