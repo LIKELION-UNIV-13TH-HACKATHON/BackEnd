b3f942c8d3b0f9a8ba14345b652e7a7c6450b339
kwakseobang <iii6602@naver.com>
2025-08-04 23:38:09 +0900

    Feat/#9 : 정적 메소드 분리 및 제약 조건 추가

--- src/main/java/org/kwakmunsu/dingdongpang/domain/member/entity/MemberType.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/member/entity/MemberType.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +13,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.BaseEntity;
 
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"member_id", "status"})
+        }
+)
 @Builder
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,15 +26,22 @@ import org.kwakmunsu.dingdongpang.domain.BaseEntity;
 public class MemberType extends BaseEntity {
 
     @Column(nullable = false)
-    Long memberId;
+    private Long memberId;
 
     @Enumerated(EnumType.STRING)
-    MemberStatus status;
+    private MemberStatus status;
 
-    public static MemberType create(Long memberId, MemberStatus status) {
+    public static MemberType createCustomer(Long memberId) {
         return MemberType.builder()
                 .memberId(memberId)
-                .status(status)
+                .status(MemberStatus.CUSTOMER)
+                .build();
+    }
+
+    public static MemberType createMerchant(Long memberId) {
+        return MemberType.builder()
+                .memberId(memberId)
+                .status(MemberStatus.MERCHANT)
                 .build();
     }
 
