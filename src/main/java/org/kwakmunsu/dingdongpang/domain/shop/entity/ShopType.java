7c073905633a50f8d9c284a6374cbfb923015081
kwakseobang <iii6602@naver.com>
2025-08-05 17:31:25 +0900

    Feat/#12 : ShopType Enum 생

---
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/shop/entity/ShopType.java
@@ -0,0 +1,12 @@
+package org.kwakmunsu.dingdongpang.domain.shop.entity;
+
+public enum ShopType {
+
+    AGRI_FISH_LIVESTOCK, // 농수축산물
+    FOOD,                // 식료품
+    DAILY_SUPPLIES,      // 생활잡화
+    FASHION,             // 의류/패션
+    HEALTH_BEAUTY,       // 건강/미용
+    ETC,                 // 기타
+    ;
+}
