a6314a2e60f320d526264b9b0ff935a84825af96
kwakseobang <iii6602@naver.com>
2025-08-06 19:40:32 +0900

    Feat/#12 : 상인 회원 API 기능

--- src/main/java/org/kwakmunsu/dingdongpang/domain/member/controller/MemberController.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package org.kwakmunsu.dingdongpang.domain.member.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.member.controller.dto.CustomerRegisterRequest;
+import org.kwakmunsu.dingdongpang.domain.member.controller.dto.MerchantRegisterRequest;
 import org.kwakmunsu.dingdongpang.domain.member.service.MemberCommandService;
 import org.kwakmunsu.dingdongpang.domain.member.service.MemberQueryService;
+import org.kwakmunsu.dingdongpang.domain.member.service.MerchantOnboardingService;
 import org.kwakmunsu.dingdongpang.domain.member.service.dto.CheckNicknameResponse;
 import org.kwakmunsu.dingdongpang.global.annotation.AuthMember;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/members")
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
+    private final MerchantOnboardingService merchantOnboardingService;
 
     @PostMapping("/customers")
     public ResponseEntity<Void> register(
@@ -29,6 +35,18 @@ public class MemberController {
             @AuthMember Long memberId
     ) {
         memberCommandService.registerCustomer(request.toServiceRequest(memberId));
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/merchants")
+    public ResponseEntity<Void> register(
+            @Valid @RequestPart MerchantRegisterRequest request,
+            @RequestPart(value = "mainImage", required = false) MultipartFile mainImage,
+            @RequestPart(value = "imageFiles", required = false) List<MultipartFile> imageFiles,
+            @AuthMember Long memberId
+    ) {
+        merchantOnboardingService.register(request.toServiceRequest(mainImage, imageFiles, memberId));
 
         return ResponseEntity.ok().build();
     }
--- src/test/java/org/kwakmunsu/dingdongpang/domain/member/controller/MemberControllerTest.java
+++ src/test/java/org/kwakmunsu/dingdongpang/domain/member/controller/MemberControllerTest.java
@@ -3,16 +3,29 @@ package org.kwakmunsu.dingdongpang.domain.member.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kwakmunsu.dingdongpang.ControllerTestSupport;
 import org.kwakmunsu.dingdongpang.domain.member.controller.dto.CustomerRegisterRequest;
+import org.kwakmunsu.dingdongpang.domain.member.controller.dto.MerchantRegisterRequest;
+import org.kwakmunsu.dingdongpang.domain.member.controller.dto.OperationTimeRequest;
+import org.kwakmunsu.dingdongpang.domain.member.controller.dto.ShopRegisterRequest;
 import org.kwakmunsu.dingdongpang.domain.member.service.dto.CheckNicknameResponse;
+import org.kwakmunsu.dingdongpang.domain.shop.entity.ShopType;
 import org.kwakmunsu.dingdongpang.global.TestMember;
+import org.kwakmunsu.dingdongpang.global.exception.DuplicationException;
+import org.kwakmunsu.dingdongpang.global.exception.dto.ErrorStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MvcResult;
 
 class MemberControllerTest extends ControllerTestSupport {
 
@@ -60,4 +73,98 @@ class MemberControllerTest extends ControllerTestSupport {
                 .extractingPath("$.existsNickname").isEqualTo(true);
     }
 
+    @TestMember
+    @DisplayName("상인 회원을 등록한다.")
+    @Test
+    void registerMerchant() throws IOException {
+        var shopRegisterRequest = getShopRegisterRequest();
+        var request = new MerchantRegisterRequest("nickname", "123456", "kwak", shopRegisterRequest);
+
+        var mainImage = new MockMultipartFile(
+                "mainImage",
+                "profile.jpg",
+                "image/jpeg",
+                "profile image content".getBytes()
+        );
+        var imageFile1 = new MockMultipartFile(
+                "imageFiles",
+                "profile.jpg",
+                "image/jpeg",
+                "profile image content".getBytes()
+        );
+        var imageFile2 = new MockMultipartFile(
+                "imageFiles",
+                "profile.jpg",
+                "image/jpeg",
+                "profile image content".getBytes()
+        );
+        var requestPart = new MockMultipartFile(
+                "request",
+                "request.json",
+                "application/json",
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+
+        MvcResult result = mvcTester.perform(multipart("/members/merchants")
+                .file(requestPart)
+                .file(mainImage)
+                .file(imageFile1)
+                .file(imageFile2)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .content(objectMapper.writeValueAsString(request))
+
+        ).getMvcResult();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
+
+    @TestMember
+    @DisplayName("상인 회원을 등록한다.")
+    @Test
+    void failRegisterMerchant() throws IOException {
+        var shopRegisterRequest = getShopRegisterRequest();
+        var request = new MerchantRegisterRequest("nickname", "123456", "kwak", shopRegisterRequest);
+
+        var requestPart = new MockMultipartFile(
+                "request",
+                "request.json",
+                "application/json",
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+        doThrow(new DuplicationException(ErrorStatus.DUPLICATE_MERCHANT))
+                .when(merchantOnboardingService)
+                .register(any());
+        MvcResult result = mvcTester.perform(multipart("/members/merchants")
+                .file(requestPart)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .content(objectMapper.writeValueAsString(request))
+
+        ).getMvcResult();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(409);
+    }
+
+    private ShopRegisterRequest getShopRegisterRequest() {
+        OperationTimeRequest mondayOperation = new OperationTimeRequest(
+                DayOfWeek.MONDAY,
+                "09:00",
+                "22:00",
+                false
+        );
+        OperationTimeRequest tuesdayOperation = new OperationTimeRequest(
+                DayOfWeek.TUESDAY,
+                "09:00",
+                "22:00",
+                false
+        );
+
+        return new ShopRegisterRequest(
+                "My Shop",                                  // shopName
+                ShopType.FOOD,                              // shopType (예: enum)
+                "010-1234-5678",                            // shopPhoneNumber
+                "경기도 광주시 경충대로1461번길 12-4 코오롱 세이브 프라자 202호", // address
+                List.of(mondayOperation, tuesdayOperation)
+        );
+
+    }
 }
