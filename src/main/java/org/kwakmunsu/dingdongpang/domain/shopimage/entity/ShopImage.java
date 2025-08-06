3d7a09df7102ec15bc45242bcb39f06d2c7f3e77
kwakseobang <iii6602@naver.com>
2025-08-06 19:35:28 +0900

    Feat/#12 : ShopImage Entity 생성

--- src/main/java/org/kwakmunsu/dingdongpang/domain/shopimage/entity/ShopImage.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/shopimage/entity/ShopImage.java
@@ -1,9 +1,27 @@
 package org.kwakmunsu.dingdongpang.domain.shopimage.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.BaseEntity;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class ShopImage extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long shopId;
+
+    @Column(nullable = false)
+    private String image;
+
+    public static ShopImage create(Long shopId, String image) {
+        return new ShopImage(shopId, image);
+    }
 
 }
