959fb588dce1c705efba75feac3540aef8e8a239
kwakseobang <iii6602@naver.com>
2025-08-07 17:16:46 +0900

    Feat/#17 : Menu Entity 상태 및 행위 추가

--- src/main/java/org/kwakmunsu/dingdongpang/domain/menu/entity/Menu.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/menu/entity/Menu.java
@@ -1,9 +1,52 @@
 package org.kwakmunsu.dingdongpang.domain.menu.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.BaseEntity;
+import org.kwakmunsu.dingdongpang.domain.shop.entity.Shop;
 
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Menu extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    @Column(nullable = false)
+    private String name;
+
+    private int price;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    private String image;
+
+    public static Menu create(Shop shop, String name, int price, String description, String image) {
+        if (price < 0)  throw new IllegalArgumentException("가격은 0원 이상이어야 합니다.");
+
+        return Menu.builder()
+                .shop(shop)
+                .name(name)
+                .price(price)
+                .description(description)
+                .image(image)
+                .build();
+    }
+
+    public void updateMenu() {
+    }
 
 }
