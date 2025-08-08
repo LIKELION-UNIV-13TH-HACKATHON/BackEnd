9d7383423d8c6498a3039282fe424b07ed9cce1c
kwakseobang <iii6602@naver.com>
2025-08-08 19:15:30 +0900

    Feat/#26 : SubscribeShop Entity Update

--- src/main/java/org/kwakmunsu/dingdongpang/domain/subscribeshop/entity/SubscribeShop.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/subscribeshop/entity/SubscribeShop.java
@@ -1,9 +1,27 @@
 package org.kwakmunsu.dingdongpang.domain.subscribeshop.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.BaseEntity;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class SubscribeShop extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long shopId;
+
+    public static SubscribeShop create(Long memberId, Long shopId) {
+        return new SubscribeShop(memberId, shopId);
+    }
 
 }
