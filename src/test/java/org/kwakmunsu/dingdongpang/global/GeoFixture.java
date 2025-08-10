e3997a799d6ef2ee3059ad8a4150167c81f6f79f
kwakseobang <iii6602@naver.com>
2025-08-10 21:02:33 +0900

    Chore/#34 : 매개변수 위치 변경

--- src/test/java/org/kwakmunsu/dingdongpang/global/GeoFixture.java
+++ src/test/java/org/kwakmunsu/dingdongpang/global/GeoFixture.java
@@ -12,7 +12,7 @@ public final class GeoFixture {
 
     private static final GeometryFactory GF = new GeometryFactory(new PrecisionModel(), 4326);
 
-    public static Point createPoint(double latitude, double longitude) {
+    public static Point createPoint(double longitude, double latitude) {
         // X=longitude, Y=latitude
         return GF.createPoint(new Coordinate(longitude, latitude));
     }
