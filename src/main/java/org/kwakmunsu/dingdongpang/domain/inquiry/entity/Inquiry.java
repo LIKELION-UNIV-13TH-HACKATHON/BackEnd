9643864ce8948d7091787b0ab6eb8dcf5f2509aa
kwakseobang <iii6602@naver.com>
2025-08-11 10:57:05 +0900

    Feat/#35 : Inquiry Entity 정보 추가

--- src/main/java/org/kwakmunsu/dingdongpang/domain/inquiry/entity/Inquiry.java
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/inquiry/entity/Inquiry.java
@@ -1,9 +1,47 @@
 package org.kwakmunsu.dingdongpang.domain.inquiry.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.kwakmunsu.dingdongpang.domain.BaseEntity;
 
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Inquiry extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long shopId;
+
+    @Column(nullable = false)
+    private Long authorId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String question;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+
+    public static Inquiry create(Long shopId, Long authorId, String question) {
+        return Inquiry.builder()
+                .shopId(shopId)
+                .authorId(authorId)
+                .question(question)
+                .build();
+    }
+
+    public void registerAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void updateAnswer(String answer) {
+        this.answer = answer;
+    }
 
 }
