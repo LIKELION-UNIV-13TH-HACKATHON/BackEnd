3bf0e3e3db0c49742a935eedaba10a6f733027fb
kwakseobang <iii6602@naver.com>
2025-08-11 22:22:08 +0900

    Feat/#44 : 문의 답변 수정 기능 구현

---
+++ src/main/java/org/kwakmunsu/dingdongpang/domain/inquiry/controller/dto/InquiryAnswerModifyRequest.java
@@ -0,0 +1,23 @@
+package org.kwakmunsu.dingdongpang.domain.inquiry.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import org.kwakmunsu.dingdongpang.domain.inquiry.service.dto.request.InquiryAnswerServiceRequest;
+
+@Schema(description = "문의 답변 수정 요청 DTO")
+public record InquiryAnswerModifyRequest(
+        @Schema(description = "답변 내용", example = "괴물쥐도 아니고 쥐는 안나와요. 허위 사실 금지입니다.")
+        @NotBlank(message = "답변 내용은 필수값입니다.")
+        String answer
+) {
+
+    public InquiryAnswerServiceRequest toServiceRequest(Long shopId, Long inquiryId, Long merchantId) {
+        return InquiryAnswerServiceRequest.builder()
+                .answer(answer)
+                .shopId(shopId)
+                .inquiryId(inquiryId)
+                .merchantId(merchantId)
+                .build();
+    }
+
+}
